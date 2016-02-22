669b6d9ef5c19f27541c486ff2d2e785d3dd99f2
Max <profpepper@virgilio.it>
2016-02-22 12:55:55 -0500

    Deal with relative path for images

--- R/SimplifyPage.R
+++ R/SimplifyPage.R
@@ -1,15 +1,7 @@
-# TODO: add download of html page...
-# url <- "http://www.hardballtimes.com/the-fascinating-enigma-of-the-non-roster-invitee/"
-
-# download.file(url, "destfile.html" , quiet = FALSE, mode = "w",
-#               cacheOK = TRUE,
-#               extra = getOption("download.file.extra"))
-
 removescripts <- function(
   url = "destfile.html"
   , dest = "destfile2.html"
 ){
-# strips html page of scripts
   require(XML)
   require(xml2)
   doc <- htmlParse(url,useInternalNode=TRUE)
@@ -24,15 +16,20 @@ workimages <- function(
   , dest = "destfile3.html"
   , folder = 'img'
 ){
-  # downloads images from html page and update src entries to look locally
   require(XML)
   require(dplyr)
   require(stringr)
+  require(urltools)
   doc <- htmlParse(url,useInternalNode=TRUE)
   r <- xmlRoot(doc)
   img <- xpathSApply(r, "//img")
   urls <- sapply(img, function(x) xmlAttrs(x)["src"])
-  imgnames <- sapply(urls, function(x){
+  urlsdf <- url_parse(urls)
+  # TODO: there's a url_parse in xml2, slightly different, change to use that one
+  maindomain <- urltools::urlsdf %>% group_by(scheme, domain) %>% tally() %>% arrange(desc(n)) %>% filter(domain != "") %>% filter(row_number()==1) %>% mutate(fulldomain = paste0(scheme, "://", domain))
+  maindomain <- maindomain$fulldomain
+
+  imgnames <- sapply(urlsdf$path, function(x){
     spl <- str_split(x, "/")
     nm <- spl[[1]][length(spl[[1]])]
     return(nm)
@@ -46,9 +43,11 @@ workimages <- function(
   })
   
   sapply(1:length(img), function(i){
-    # TODO: make it work for relative links too
-    if(substr(urls[i], 1, 4)=="http" & str_count(imgnames[i], fixed("?")) == 0) 
-      download.file(urls[i], paste(folder, imgnames[i], sep="/") , quiet = FALSE, mode="wb")
+      if(urlsdf$domain[i] != ""){
+        try(download.file(urls[i], paste(folder, imgnames[i], sep="/") , quiet = FALSE, mode="wb"), silent = T)
+      } else { #if(urlsdf$domain[i] != "")
+        try(download.file(paste0(maindomain, urls[i]), paste(folder, imgnames[i], sep="/") , quiet = FALSE, mode="wb"), silent = T)
+      } #if(urlsdf$domain[i] != "")
     })
   
   saveXML(r, dest)
