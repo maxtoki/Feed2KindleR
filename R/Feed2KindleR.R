1f99cb57fef69ab7ca137882c62277b3012797fa
Max <profpepper@virgilio.it>
2016-02-23 08:05:57 -0500

    fixes on download / process part

--- R/Feed2KindleR.R
+++ R/Feed2KindleR.R
@@ -61,7 +61,8 @@ Feed2KindleR <- function(
   feedData <- list(feed=feedname, description=feeddescr, articles=feedarticles)
   
   ### download and process html files ############
-  sapply(feedData$feedarticles, function(item){
+  sapply(1:nrow(feedData$articles), function(itemn){
+    item <- feedData$articles[itemn,]
     urlparts <- url_parse(item$link)
     pathelements <- urlparts$path %>% str_replace("^/", "") %>% str_replace("/$", "")
     filename <- pathelements[length(pathelements)]
@@ -70,51 +71,56 @@ Feed2KindleR <- function(
     
     # if file not in the html folder, download it
     filepath <- paste(fld_html, filename, sep="/")
-    if(length(setdiff(filename, list.files(fld_html))) > 0)
-      download.file(item$link,  filepath, quiet = FALSE, mode = "w", cacheOK = TRUE, extra = getOption("download.file.extra"))
-    
-    # remove script and head tags
-    doc <- htmlParse(filepath ,useInternalNode=TRUE)
-    r <- xmlRoot(doc)
-    removeNodes(xpathSApply(r, "//script"))
-    removeNodes(xpathSApply(r, "//head"))
-    saveXML(r, filepath)
-    
-    # save images locally and modify html accordingly
-    img <- xpathSApply(r, "//img")
-    urls <- sapply(img, function(x) xmlAttrs(x)["src"])
-    urlsdf <- url_parse(urls)
-    # images names
-    imgnames <- sapply(urlsdf$path, function(x){
-      spl <- str_split(x, "/")
-      nm <- spl[[1]][length(spl[[1]])]
-      return(nm)
-    })
-    # modify the html file to look into the local img folder
-    sapply(1:length(img), function(i){
-      ond <- img[[i]]
-      nnd <- ond
-      xmlAttrs(ond)["src"] <- paste(fld_img, imgnames[i], sep="/")
-      replaceNodes(ond, nnd)
-    })
-    saveXML(r, filepath)
-    # download images locally (if not already there)
-    sapply(1:length(img), function(i){
-      if(length(setdiff(imgnames[i], list.files(fld_img)))){
-        if(urlsdf$server[i] != ""){
-          try(download.file(urls[i], paste(fld_img, imgnames[i], sep="/") , quiet = FALSE, mode="wb"), silent = T)
-        } else { #if(urlsdf$domain[i] != "")
-          try(download.file(paste0(urlparts$scheme, "://", urlparts$server, "/", urls[i]), paste(fld_img, imgnames[i], sep="/") , quiet = FALSE, mode="wb"), silent = T)
-        } #if(urlsdf$domain[i] != "")
-      } #if(length(setdiff(imgnames[i], list.files(fld_img))))
-    })
+    if(length(setdiff(filename, list.files(fld_html))) > 0){
+      try(download.file(item$link,  filepath, quiet = FALSE, mode = "w", cacheOK = TRUE, extra = getOption("download.file.extra")))
+      
+      # remove script and head tags
+      doc <- htmlParse(filepath ,useInternalNode=TRUE)
+      r <- xmlRoot(doc)
+      removeNodes(xpathSApply(r, "//script"))
+      removeNodes(xpathSApply(r, "//head"))
+      saveXML(r, filepath)
+      
+      # save images locally and modify html accordingly
+      img <- xpathSApply(r, "//img")
+      urls <- sapply(img, function(x) xmlAttrs(x)["src"])
+      urlsdf <- url_parse(urls)
+      # images names
+      imgnames <- sapply(urlsdf$path, function(x){
+        spl <- str_split(x, "/")
+        nm <- spl[[1]][length(spl[[1]])]
+        return(nm)
+      })
+      # modify the html file to look into the local img folder
+      sapply(1:length(img), function(i){
+        ond <- img[[i]]
+        nnd <- ond
+        xmlAttrs(ond)["src"] <- paste(fld_img, imgnames[i], sep="/")
+        replaceNodes(ond, nnd)
+      })
+      saveXML(r, filepath)
+      # download images locally (if not already there)
+      sapply(1:length(img), function(i){
+        if(length(setdiff(tolower(imgnames[i]), tolower(list.files(fld_img))))>0){
+          if(urlsdf$server[i] != ""){
+            try(download.file(urls[i], paste(fld_img, imgnames[i], sep="/") , quiet = FALSE, mode="wb"), silent = T)
+          } else { #if(urlsdf$domain[i] != "")
+            try(download.file(paste0(urlparts$scheme[1], "://", urlparts$server[1], "/", urls[i]), paste(fld_img, imgnames[i], sep="/") , quiet = FALSE, mode="wb"), silent = T)
+          } #if(urlsdf$domain[i] != "")
+        } #if(length(setdiff(imgnames[i], list.files(fld_img))))
+      })
+      
+    } #if(length(setdiff(filename, list.files(fld_html))) > 0)
   }) #sapply(feedData, function(item)
   
-  # TODO: clean up old html and image files (like 2 weeks?)
+  # TODO: clean up old html and image files (like 2 weeks?) using file.info() function
   
   # TODO: generate OPF file
+  
+  
   # TODO: generate NCX file
   # TODO: launch KindleGen to generate the mobi file
-  # TODO: email mobi file to kindle address
+  # TODO: email mobifile to kindle address
   
 } #Feed2KindleR
+
